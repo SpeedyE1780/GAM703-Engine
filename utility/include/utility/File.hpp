84a8d415cc585dc6e035f563f33cb21079ea3ccd
Sandro Saab <ss298346@falmouth.ac.uk>
2023-11-30 17:24:37 +0000

    Add file read function

---
+++ utility/include/utility/File.hpp
@@ -0,0 +1,12 @@
+#ifndef GAM703_ENGINE_UTILITY_FILE_FILE_HPP
+#define GAM703_ENGINE_UTILITY_FILE_FILE_HPP
+
+#include <filesystem>
+#include <string>
+
+namespace gam703::engine::utility
+{
+	std::string readFile(const std::filesystem::path& filePath);
+} // namespace gam703::engine::utility
+
+#endif // GAM703_ENGINE_UTILITY_FILE_FILE_HPP
