56023ad89764fdd24c75a81667831033f890920e
Sandro Saab <ss298346@falmouth.ac.uk>
2023-12-04 15:32:17 +0000

    Create core library

---
+++ core/include/core/Engine.hpp
@@ -0,0 +1,12 @@
+#ifndef GAM703_ENGINE_CORE_ENGINE_HPP
+#define GAM703_ENGINE_CORE_ENGINE_HPP
+
+namespace gam703::engine::core
+{
+	class Engine
+	{
+
+	};
+}
+
+#endif // GAM703_ENGINE_CORE_ENGINE_HPP
---
+++ core/src/Engine.cpp
@@ -0,0 +1,6 @@
+#include <core/Engine.hpp>
+
+namespace gam703::engine::core
+{
+
+} //gam703::engine::core
