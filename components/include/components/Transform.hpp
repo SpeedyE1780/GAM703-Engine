24363c7ea2997a4e478c79ac464acbcefd3aeba6
Sandro Saab <ss298346@falmouth.ac.uk>
2023-12-08 10:05:27 +0000

    Add getters/setters for Transform class

--- components/include/components/Transform.hpp
+++ components/include/components/Transform.hpp
@@ -12,14 +12,27 @@ namespace gam703::engine::components
 	public:
 		Transform(glm::vec3 position = glm::vec3(0, 0, 0), glm::vec3 rotation = glm::vec3(0, 0, 0), glm::vec3 scale = glm::vec3(1, 1, 1));
 
-		glm::vec3 getPosition() const { return m_position; }
-		glm::quat getRotation() const { return m_rotation; }
-		glm::vec3 getScale() const { return m_scale; }
+		const glm::vec3& getPosition() const { return m_position; }
+		const glm::quat& getRotation() const { return m_rotation; }
+		const glm::vec3& getScale() const { return m_scale; }
+		const glm::mat4& getTransformationMatrix() const { return m_transformMatrix; }
+
+		void calculateTransformMatrix();
+
+		void setPosition(const glm::vec3& position);
+		void setPosition(float x, float y, float z);
+		void setRotation(const glm::quat& rotation);
+		void setRotation(const glm::vec3& eulerAngles);
+		void setRotation(float x, float y, float z);
+		void setScale(const glm::vec3& scale);
+		void setScale(float x, float y, float z);
 
 	private:
 		glm::vec3 m_position;
 		glm::quat m_rotation;
 		glm::vec3 m_scale;
+		glm::mat4 m_transformMatrix;
+		bool m_shouldCalculateTransform;
 	};
 }
 
--- components/src/Transform.cpp
+++ components/src/Transform.cpp
@@ -1,8 +1,64 @@
 #include <components/Transform.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 namespace gam703::engine::components
 {
-	Transform::Transform(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale) : m_position(position), m_rotation(rotation), m_scale(scale)
+	Transform::Transform(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale) : m_position(position), m_rotation(rotation), m_scale(scale), m_transformMatrix(glm::mat4(1)), m_shouldCalculateTransform(true)
 	{
+		calculateTransformMatrix();
+	}
+
+	void Transform::calculateTransformMatrix()
+	{
+		if (m_shouldCalculateTransform)
+		{
+			m_transformMatrix = glm::mat4(1);
+			m_transformMatrix = glm::translate(m_transformMatrix, m_position);
+			m_transformMatrix = m_transformMatrix * glm::toMat4(m_rotation);
+			m_transformMatrix = glm::scale(m_transformMatrix, m_scale);
+			m_shouldCalculateTransform = false;
+		}
+	}
+
+	void Transform::setPosition(const glm::vec3& position)
+	{
+		m_position = position;
+		m_shouldCalculateTransform = true;
+	}
+
+	void Transform::setPosition(float x, float y, float z)
+	{
+		m_position = glm::vec3(x, y, z);
+		m_shouldCalculateTransform = true;
+	}
+
+	void Transform::setRotation(const glm::quat& rotation)
+	{
+		m_rotation = rotation;
+		m_shouldCalculateTransform = true;
+	}
+
+	void Transform::setRotation(const glm::vec3& eulerAngles)
+	{
+		m_rotation = glm::quat(eulerAngles);
+		m_shouldCalculateTransform = true;
+	}
+
+	void Transform::setRotation(float x, float y, float z)
+	{
+		m_rotation = glm::quat(1, x, y, z);
+		m_shouldCalculateTransform = true;
+	}
+
+	void Transform::setScale(const glm::vec3& scale)
+	{
+		m_scale = scale;
+		m_shouldCalculateTransform = true;
+	}
+
+	void Transform::setScale(float x, float y, float z)
+	{
+		m_scale = glm::vec3(x, y, z);
+		m_shouldCalculateTransform = true;
 	}
 }
