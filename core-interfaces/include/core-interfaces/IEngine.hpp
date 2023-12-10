84b93ebbd95e01342db348ed0caa16c5469cee45
Sandro Saab <ss298346@falmouth.ac.uk>
2023-12-10 22:56:16 +0000

    Create core interface library

---
+++ core-interfaces/include/core-interfaces/IEngine.hpp
@@ -0,0 +1,13 @@
+#ifndef GAM703_ENGINE_CORE_INTERFACES_IENGINE_HPP
+#define GAM703_ENGINE_CORE_INTERFACES_IENGINE_HPP
+
+namespace gam703::engine::core_interface
+{
+	class IEngine
+	{
+	public:
+		virtual ~IEngine() = default;
+	};
+}
+
+#endif // GAM703_ENGINE_CORE_INTERFACES_IENGINE_HPP
