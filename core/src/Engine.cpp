ba94b9dbca91f7674ef2799e3f9cf7d09e14a2c8
Sandro Saab <ss298346@falmouth.ac.uk>
2023-12-11 11:02:41 +0000

    Add multiple models to increase memory

--- core/src/Engine.cpp
+++ core/src/Engine.cpp
@@ -71,13 +71,24 @@ namespace gam703::engine::core
 	void Engine::run()
 	{
 		graphic::Model ourModel("resources/Models/backpack/backpack.obj");
-		graphic::Shader shader = graphic::createDefaultShader();
+		graphic::Model ourModel2("resources/Models/backpack/backpack.obj");
+		graphic::Model ourModel3("resources/Models/backpack/backpack.obj");
+		graphic::Model ourModel4("resources/Models/backpack/backpack.obj");
 
 		components::Transform cameraTransform(this, glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0, glm::radians(-90.0f), 0));
 		m_mainCamera = cameraTransform.addComponent<components::Camera>();
 
-		components::Transform backpackTransform{ this };
-		auto* backpackRenderer = backpackTransform.addComponent<components::Renderer>(ourModel, shader);
+		components::Transform backpackTransform{ this, glm::vec3(0.0f, 0.0f, 0.0f) };
+		backpackTransform.addComponent<components::Renderer>(ourModel);
+
+		components::Transform backpackTransform2{ this, glm::vec3(1.0f, 1.0f, 0.0f) };
+		backpackTransform2.addComponent<components::Renderer>(ourModel2);
+
+		components::Transform backpackTransform3{ this, glm::vec3(2.0f, 2.0f, 0.0f) };
+		backpackTransform3.addComponent<components::Renderer>(ourModel3);
+
+		components::Transform backpackTransform4{ this, glm::vec3(3.0f, 3.0f, 0.0f) };
+		backpackTransform4.addComponent<components::Renderer>(ourModel4);
 
 		m_sceneRenderer.setMainCamera(m_mainCamera);
 
