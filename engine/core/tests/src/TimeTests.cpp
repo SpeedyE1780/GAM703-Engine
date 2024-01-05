d3f4d7148d2f8e22faf218f8cb32f0567bcc483d
Sandro Saab <ss298346@falmouth.ac.uk>
2024-01-05 19:10:08 +0000

    Refactor core-tests

--- engine/core/tests/src/TimeTests.cpp
+++ engine/core/tests/src/TimeTests.cpp
@@ -1,84 +1,87 @@
-#include <engine/core-interfaces/Time.hpp>
+#include <engine/core/Time.hpp>
 
 #include <gtest/gtest.h>
 
-TEST(TimeTests, SetTime)
+namespace gam703::engine::core::tests
 {
-	const double expected = 20;
-	gam703::engine::core_interface::Time time(expected);
+	TEST(TimeTests, SetTime)
+	{
+		const double expected = 20;
+		Time time(expected);
 
-	EXPECT_EQ(expected, time.getTime());
-}
+		EXPECT_EQ(expected, time.getTime());
+	}
 
-TEST(TimeTests, UpdateDeltaTime)
-{
-	const double initialTime = 20;
-	gam703::engine::core_interface::Time time(initialTime);
+	TEST(TimeTests, UpdateDeltaTime)
+	{
+		const double initialTime = 20;
+		Time time(initialTime);
 
-	ASSERT_EQ(initialTime, time.getTime());
+		ASSERT_EQ(initialTime, time.getTime());
 
-	const double timeStep = 5;
-	const double newTime = initialTime + timeStep;
-	time.processTime(newTime);
+		const double timeStep = 5;
+		const double newTime = initialTime + timeStep;
+		time.processTime(newTime);
 
-	EXPECT_EQ(newTime, time.getTime());
-	EXPECT_EQ(timeStep, time.getDeltaTime());
-}
+		EXPECT_EQ(newTime, time.getTime());
+		EXPECT_EQ(timeStep, time.getDeltaTime());
+	}
 
-TEST(TimeTests, SetTimeScale)
-{
-	const double initialTime = 20;
-	const double timeScale = 2;
-	gam703::engine::core_interface::Time time(initialTime, timeScale);
+	TEST(TimeTests, SetTimeScale)
+	{
+		const double initialTime = 20;
+		const double timeScale = 2;
+		Time time(initialTime, timeScale);
 
-	ASSERT_EQ(initialTime, time.getTime());
-	EXPECT_EQ(timeScale, time.getTimeScale());
-}
+		ASSERT_EQ(initialTime, time.getTime());
+		EXPECT_EQ(timeScale, time.getTimeScale());
+	}
 
-TEST(TimeTests, UpdateScaledDeltaTime)
-{
-	const double initialTime = 20;
-	const double timeScale = 2;
-	gam703::engine::core_interface::Time time(initialTime, timeScale);
+	TEST(TimeTests, UpdateScaledDeltaTime)
+	{
+		const double initialTime = 20;
+		const double timeScale = 2;
+		Time time(initialTime, timeScale);
 
-	ASSERT_EQ(initialTime, time.getTime());
-	ASSERT_EQ(timeScale, time.getTimeScale());
+		ASSERT_EQ(initialTime, time.getTime());
+		ASSERT_EQ(timeScale, time.getTimeScale());
 
-	const double timeStep = 5;
-	const double newTime = initialTime + timeStep;
-	time.processTime(newTime);
+		const double timeStep = 5;
+		const double newTime = initialTime + timeStep;
+		time.processTime(newTime);
 
-	ASSERT_EQ(newTime, time.getTime());
+		ASSERT_EQ(newTime, time.getTime());
 
-	const double scaledTimeStep = timeStep * timeScale;
-	EXPECT_EQ(scaledTimeStep, time.getDeltaTime());
-	EXPECT_EQ(timeStep, time.getUnscaledDeltaTime());
-}
+		const double scaledTimeStep = timeStep * timeScale;
+		EXPECT_EQ(scaledTimeStep, time.getDeltaTime());
+		EXPECT_EQ(timeStep, time.getUnscaledDeltaTime());
+	}
 
-TEST(TimeTests, UpdateTimeScaleThenRevert)
-{
-	const double initialTime = 20;
-	const double timeScale = 2;
-	gam703::engine::core_interface::Time time(initialTime, timeScale);
+	TEST(TimeTests, UpdateTimeScaleThenRevert)
+	{
+		const double initialTime = 20;
+		const double timeScale = 2;
+		Time time(initialTime, timeScale);
 
-	ASSERT_EQ(initialTime, time.getTime());
-	ASSERT_EQ(timeScale, time.getTimeScale());
+		ASSERT_EQ(initialTime, time.getTime());
+		ASSERT_EQ(timeScale, time.getTimeScale());
 
-	const double timeStep = 5;
-	double newTime = initialTime + timeStep;
-	time.processTime(newTime);
+		const double timeStep = 5;
+		double newTime = initialTime + timeStep;
+		time.processTime(newTime);
 
-	ASSERT_EQ(newTime, time.getTime());
+		ASSERT_EQ(newTime, time.getTime());
 
-	const double scaledTimeStep = timeStep * timeScale;
-	EXPECT_EQ(scaledTimeStep, time.getDeltaTime());
-	EXPECT_EQ(timeStep, time.getUnscaledDeltaTime());
+		const double scaledTimeStep = timeStep * timeScale;
+		EXPECT_EQ(scaledTimeStep, time.getDeltaTime());
+		EXPECT_EQ(timeStep, time.getUnscaledDeltaTime());
 
-	time.setTimeScale(1);
-	newTime += timeStep;
-	time.processTime(newTime);
+		time.setTimeScale(1);
+		newTime += timeStep;
+		time.processTime(newTime);
 
-	EXPECT_EQ(newTime, time.getTime());
-	EXPECT_EQ(timeStep, time.getDeltaTime());
-	EXPECT_EQ(timeStep, time.getUnscaledDeltaTime());
+		EXPECT_EQ(newTime, time.getTime());
+		EXPECT_EQ(timeStep, time.getDeltaTime());
+		EXPECT_EQ(timeStep, time.getUnscaledDeltaTime());
+	}
 }
