b9571508b87b0c6ae52926873776acd9a6e260cf
Sandro Saab <ss298346@falmouth.ac.uk>
2024-01-06 22:46:25 +0000

    Add Checkbox documentation

--- engine/gui/include/engine/gui/CheckBox.hpp
+++ engine/gui/include/engine/gui/CheckBox.hpp
@@ -8,17 +8,25 @@
 
 namespace gam703::engine::gui
 {
+	/// @brief Checkbox on screen
 	class ENGINE_GUI_API Checkbox : public GUIElement
 	{
 	public:
+		/// @brief Create a checkbox on the screen
+		/// @param label Label written next to check box
+		/// @param initialState Initial checkbox state
 		Checkbox(const std::string& label, bool initialState = false);
 
+		/// @brief Return if checkbox is checked
+		/// @return True if checkbox is checked
 		bool isChecked() const { return m_state; }
 
 	protected:
+		/// @brief Render the element on the screen
 		virtual void renderElement() const override;
 
 	private:
+		/// @brief If checkbox is checked or no
 		mutable bool m_state;
 	};
 }
