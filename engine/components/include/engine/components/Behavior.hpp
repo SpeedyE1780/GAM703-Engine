ea97fa7c2d564d496ab86dadc83760d1f8672e80
Sandro Saab <ss298346@falmouth.ac.uk>
2024-01-06 20:29:59 +0000

    Add Behavior documentation

--- engine/components/include/engine/components/Behavior.hpp
+++ engine/components/include/engine/components/Behavior.hpp
@@ -6,13 +6,22 @@
 
 namespace gam703::engine::components
 {
+	/// @brief Behavior is the Parent class of components that will be updated every frame
 	class ENGINE_COMPONENTS_API Behavior : public Component
 	{
 	public:
+		/// @brief Create a behavior that this is owned by the transform
+		/// @param transform The Transform owning this behavior
 		Behavior(Transform& transform);
 		virtual ~Behavior() = default;
 
+		/// @brief Update the behavior every frame
+		/// @param deltaTime Elapsed time between this frame and the last frame
 		virtual void tick(float deltaTime) = 0;
+
+		/// @brief Create a copy attached to the new transform
+		/// @param transform The new transform that will own the clone
+		/// @return The cloned Behavior belonging to the transform
 		virtual Behavior* clone(Transform& transform) const override = 0;
 	};
 }
